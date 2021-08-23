bd163ea37b2e71ab6e598e98bfe8a8ac57a0c9ac
Pham Xuan Tung <toilatung52@gmail.com>
2021-08-23 11:27:28 +0700

    lam forgot password, chia layout cho bo auth

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">Reset hihi</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -62,4 +59,4 @@
         </div>
     </div>
 </div>
-@endsection
+
