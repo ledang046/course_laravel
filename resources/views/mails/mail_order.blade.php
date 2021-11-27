f1c72486fe499f9508fbbb92b094477782898291
tungtt <toilatung52@gmail.com>
2021-11-27 23:53:44 +0700

    chỉnh sửa form email

--- resources/views/mails/mail_order.blade.php
+++ resources/views/mails/mail_order.blade.php
@@ -2,7 +2,8 @@
    <h2>{{ $data['type'] }}</h2>
    <p>{{ $data['content'] }} - Team 13</p>
    <h1 style="color:#20c997;">Welcomes to KOURSES</h1>
-   <p>Your course: {{ $data['productName'] }}</p>
-   <p>Total: {{ $data['orderPrice'] }}</p>
-   <p>Time: 2 & 4 & 6</p>
+   <h3>Your course: {{ $data['productName'] }}</h3>
+   <h3>Total: {{ number_format($data['orderPrice']) }}</h3>
+   <h3>Days: 2 & 4 & 6</h3>
+   <h3>Time: 6 pm</h3>
 </div>
