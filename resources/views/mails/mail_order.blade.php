35548c6eb0bf2adafbe2703dd084278f294c8005
tungtt <toilatung52@gmail.com>
2021-11-27 22:14:55 +0700

    Css lại form email

--- resources/views/mails/mail_order.blade.php
+++ resources/views/mails/mail_order.blade.php
@@ -1,6 +1,6 @@
 <div>
    <h2>{{ $data['type'] }}</h2>
    <p>{{ $data['content'] }}</p>
-   <p>Welcomes to KOURSES</p>
+   <h1 style="color:#20c997;">Welcomes to KOURSES</h1>
    <a href="https://genk.vn/"></a>
 </div>
