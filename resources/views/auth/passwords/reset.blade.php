320b139f37b6ae3c5514ccd00e0c9e7a4ff6457f
ledang046 <ledang046@gmail.com>
2021-11-30 01:19:55 +0700

    lai finish a

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,8 +1,23 @@
+@extends("auth.layout_auth")
+
+@section('title')
+
+@endsection
+
+@section('title_logo')
+<img src="{{asset('backend/images/logo1.png')}}" alt="Logo">
+@endsection
+@section('content')
+<style>
+    .form-control {
+        width:500px;
+    }
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Reset hihi</div>
+                <div class="card-header">Reset password</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -12,8 +27,7 @@
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -27,7 +41,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -41,7 +55,7 @@
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
@@ -59,4 +73,5 @@
         </div>
     </div>
 </div>
+@endsection
 
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Course | Admin</title>
+    <title>Kourse | Admin</title>
     <meta name="description" content="Course | Admin">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
